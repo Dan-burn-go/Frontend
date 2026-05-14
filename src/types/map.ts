9b3ef57527bc321b2a4ba1f3da72d51f4c1191a6
sujin0530 <lucy4907@naver.com>
2026-05-14 13:35:43 +0900

    feat: CultureEvent 타입 및 fetchCultureEvents API 추가

---
+++ src/types/map.ts
@@ -0,0 +1,12 @@
+export interface CultureEvent {
+  title: string;
+  place: string;
+  codename: string;
+  startDate: string;
+  endDate: string;
+  useFee: string | null;
+  orgLink: string;
+  mainImg: string;
+  latitude: number;
+  longitude: number;
+}
